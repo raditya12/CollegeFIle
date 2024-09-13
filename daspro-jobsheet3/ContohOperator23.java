7ad508f5954f78660e357fd3c022a119fa047511
raditya12 <radityariefki5@gmail.com>
2024-09-13 13:17:25 +0700

    CollegeFile

---
+++ daspro-jobsheet3/ContohOperator23.java
@@ -0,0 +1,14 @@
+public  class ContohOperator23 {
+
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("x++ = " + x++);
+        System.out.println("Setelah evaluasi, x = " +x);
+        x = 10;
+        System.out.println("++x = " + ++x);
+        System.out.println("Setelah evaluasi, x = " + x );
+        int y = 12;
+        System.out.println(x > y || y == x && y <= x);
+        int z 
+    }
+}
---
+++ daspro-jobsheet3/ContohVariabel23.java
@@ -0,0 +1,18 @@
+public  class ContohVariabel23 {
+
+    public static void main(String[] args) {
+        String salahSatuHobySayaAdalah = "bermain petak umpet";
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte _umurSayaSekarang = 20;
+        double $ipk = 3.24, tinggi = 1.78;
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+
+
+    }
+}
